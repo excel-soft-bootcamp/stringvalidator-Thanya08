fbb1a4cadfc0622ffc54cf112082ab2fe0d9cab2
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-06-29 14:40:37 +0530

    Add files via upload

---
+++ StringValidator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String data = "excel";
+            NullChecking checking = new NullChecking();
+            StringValidator validate = new StringValidator(checking);
+            validate.IsValidString(data);
+
+            LengthChecking length = new LengthChecking();
+            StringValidator _length = new StringValidator(length);
+            _length.IsValidString(data);
+
+            StringStartsWith starts = new StringStartsWith();
+            StringValidator _starts = new StringValidator(starts);
+            _starts.IsValidString(data);
+        }
+    }
+}
